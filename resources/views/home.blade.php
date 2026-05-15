4cdfe14c5e5db411f4dc323648e68a5d2a9e7c63
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-15 12:53:47 +0200

    print comics list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,27 @@
 @extends('layouts.master')
 
 @section('jumbotron')
-
 @endsection
 
+@php
+$comics = config('comics');
+@endphp
+
 @section('main-content')
+
+<div class="row row-cols-6">
+    @foreach($comics as $comic)
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">{{ $comic['series']}}</p>
+            </div>
+        </div>
+    </div>
+
+    @endforeach
+</div>
 
 @endsection
 
