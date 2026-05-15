8a62d55476b1352d4194c3591913f4ce9c106a68
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-15 14:06:29 +0200

    set component content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,14 +12,15 @@ $comics = config('comics');
 <div class="row row-cols-6 gy-4 p-4">
     @foreach($comics as $comic)
     <div class="col bg-dark">
-        <div class="card h-100 bg-dark text-white">
-            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <p class="card-text">{{ $comic['series']}}</p>
+        <x-card>
+            <div class="card h-100 bg-dark text-white">
+                <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <p class="card-text">{{ $comic['series']}}</p>
+                </div>
             </div>
-        </div>
-    </div>
-
+        </x-card>
+    </div> 
     @endforeach
 </div>
 
