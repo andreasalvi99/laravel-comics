d0c7d08defa3b4962c2e2da72fe3ba534c011ab5
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-15 12:15:09 +0200

    create header and navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>DC Comics</title>
 </head>
 <body>
+
+    @include('partials.header')
+
     <div>
         @yield('main-content')
     </div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="d-flex justify-content-around">
+        <img src="" alt="">
+        <ul class="navbar-nav d-flex flex-row">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+            <li class="nav-item">Placheholder</li class="nav-item">
+        </ul>
+    </nav>
+</header>
