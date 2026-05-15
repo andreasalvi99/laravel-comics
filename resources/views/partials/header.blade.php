c810e20e60621b1cdeea394c53f7a3879840cd2b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-15 14:28:25 +0200

    create pages and routes for every navlink

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,38 @@
 <header>
     <x-navbar>
         <nav class="d-flex justify-content-around p-3">
-            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="dc-logo" class="navbar-logo">
+            <a href="{{ route('home') }}" class="nav-link"><img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="dc-logo" class="navbar-logo"></a>
             <ul class="navbar-nav d-flex flex-row align-items-center gap-4 fw-bold">
-                <li class="nav-item">CHARACTERS</li class="nav-item">
-                <li class="nav-item">COMICS</li class="nav-item">
-                <li class="nav-item">MOVIES</li class="nav-item">
-                <li class="nav-item">TV</li class="nav-item">
-                <li class="nav-item">GAMES</li class="nav-item">
-                <li class="nav-item">COLLECTIBLES</li class="nav-item">
-                <li class="nav-item">VIDEOS</li class="nav-item">
-                <li class="nav-item">FANS</li class="nav-item">
-                <li class="nav-item">NEWS</li class="nav-item">
-                <li class="nav-item">SHOP</li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('characters') }}" class="nav-link">CHARACTERS</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('comics') }}" class="nav-link">COMICS</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('movies') }}" class="nav-link">MOVIES</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('tv') }}" class="nav-link">TV</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('games') }}" class="nav-link">GAMES</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('collectibles') }}" class="nav-link">COLLECTIBLES</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('videos') }}" class="nav-link">VIDEOS</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('fans') }}" class="nav-link">FANS</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('news') }}" class="nav-link">NEWS</a>
+                </li class="nav-item">
+                <li class="nav-item">
+                    <a href="{{ route('shop') }}" class="nav-link">SHOP</a>
+                </li class="nav-item">
             </ul>
         </nav>
     </x-navbar>
