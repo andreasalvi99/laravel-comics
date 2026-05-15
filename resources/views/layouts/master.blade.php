a1c646ccbc980862ed536e228b5619487d8bdede
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-15 12:00:52 +0200

    create layout folder and master file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC Comics</title>
+</head>
+<body>
+
+@section('main-content')
+
+@endsection
+    
+</body>
+</html>
